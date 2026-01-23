3c46bf0a3c92b32cff9b97b8cba55b972c831fb6
ravinabisht2003 <ravinabisht2003@gmail.com>
2026-01-23 07:26:09 +0530

    feat - color change website.

--- script.js
+++ script.js
@@ -2,11 +2,18 @@ let ColorChangeID = document.getElementById("boxchangeId");
 
 function ColorChangeFunction(){
 
-    let color = Math.floor(Math.random() * 16777215).toString(16).padStart(6, "0"); 
+    
     SetColorChange = setInterval(()=>{
-        (document.getElementById("boxchangeId")).style.color = "#" + color;
 
-        console.log("#" + color);
+        let color = Math.floor(Math.random() * 16777215).toString(16).padStart(6, "0"); 
+        let Bg = Math.floor(Math.random() * 16777215).toString(16).padStart(6, "0"); 
+        ColorChangeID.style.color = "#" + color;
+        ColorChangeID.style.backgroundColor = "#" + Bg;
 
-    },1000)
+        },1000)
+}
+
+function stopColorChangeFunction(){
+    clearInterval(SetColorChange);
+    SetColorChange = null;
 }
